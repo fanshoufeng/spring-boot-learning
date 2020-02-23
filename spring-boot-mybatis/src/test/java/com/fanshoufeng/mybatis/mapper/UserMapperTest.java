38a7904de8fd0dc0f8160a4d0c7036f843a052d9
Michael Fan <shoufeng.fan@gmail.com>
2020-02-23 20:29:34 +0800

    UserMapperTest运行报空指针异常: 忘记加@SpringBootTest

--- spring-boot-mybatis/src/test/java/com/fanshoufeng/mybatis/mapper/UserMapperTest.java
+++ spring-boot-mybatis/src/test/java/com/fanshoufeng/mybatis/mapper/UserMapperTest.java
@@ -5,9 +5,11 @@ import com.fanshoufeng.mybatis.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+@SpringBootTest
 class UserMapperTest {
 
     @Autowired
