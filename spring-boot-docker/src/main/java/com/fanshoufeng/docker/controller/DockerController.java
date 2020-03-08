8c31594d4dd665c72d87e1045b745f7ae7df2762
Michael Fan <shoufeng.fan@gmail.com>
2020-03-08 17:35:55 +0800

    新增spring-boot-docker

---
+++ spring-boot-docker/src/main/java/com/fanshoufeng/docker/controller/DockerController.java
@@ -0,0 +1,14 @@
+package com.fanshoufeng.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Docker";
+    }
+
+}
